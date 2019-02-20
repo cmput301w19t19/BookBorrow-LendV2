e92594a54e2b089256c988094f0ca1b75bfc3de0
carsonxie <carsontj@outlook.com>
2019-02-20 15:20:19 -0700

    add book methods

--- app/src/main/java/com/example/y/bookborrow_lendv2/SearchResultForBook.java
+++ app/src/main/java/com/example/y/bookborrow_lendv2/SearchResultForBook.java
@@ -2,6 +2,7 @@ package com.example.y.bookborrow_lendv2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SearchResultForBook extends AppCompatActivity {
 
@@ -9,5 +10,25 @@ public class SearchResultForBook extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_result_for_book);
+    }
+
+    public void newSearch(View view){
+
+
+    }
+
+    public void backHome(View view){
+
+    }
+
+    public void displayBookName(View view){
+
+
+    }
+
+
+    public void displayBookDetail(View view){
+
+        
     }
 }
