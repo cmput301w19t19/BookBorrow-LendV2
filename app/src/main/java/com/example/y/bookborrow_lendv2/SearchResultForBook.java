929bfc5374c8bb625f90fc61a9aeb03d36374d8f
carsonxie <carsontj@outlook.com>
2019-02-19 17:15:20 -0700

    add two classes

---
+++ app/src/main/java/com/example/y/bookborrow_lendv2/SearchResultForBook.java
@@ -0,0 +1,13 @@
+package com.example.y.bookborrow_lendv2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchResultForBook extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_result_for_book);
+    }
+}
