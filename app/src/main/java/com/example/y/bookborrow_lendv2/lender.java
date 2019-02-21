c89b13a8660d8cbf11fd4326491496bfc421587d
Yuant9 <yuan@Yuans-MacBook-Pro.local>
2019-02-20 20:55:14 -0700

    update lender class

--- app/src/main/java/com/example/y/bookborrow_lendv2/lender.java
+++ app/src/main/java/com/example/y/bookborrow_lendv2/lender.java
@@ -7,21 +7,52 @@ public class lender extends user {
     private ArrayList<book> lendedBook;
     private ArrayList<book> requestedBookList;
 
-    lender(){}
-    public void setLenderRating(Float rating){
+    lender() {
+    }
+
+    public void setLenderRating(Float rating) {
         this.lenderRating = rating;
     }
 
-    public float getLenderRating(){return lenderRating;}
+    public float getLenderRating() {
+        return lenderRating;
+    }
 
 
-    public void addBorrowedBook(book book){
+    public void setRequestedBookList(ArrayList<book> list) {
+        requestedBookList = list;
+    }
+
+    public void setLendedBook(ArrayList<book> list) {
+        lendedBook = list;
+    }
+
+    public ArrayList<book> getLendededBook() {
+        return lendedBook;
+    }
+
+    public ArrayList<book> getRequestedBookList() {
+        return requestedBookList;
+    }
+
+    public void addlendedBook(book book) {
         lendedBook.add(book);
     }
 
-    public void addRequestedBook(book book){
+    public void addRequestedBook(book book) {
         requestedBookList.add(book);
     }
 
-}
+    public void deleteRequestedBook(book book) {
+        requestedBookList.remove(book);
+    }
 
+    public void deleteBorrowedBook(book book) {
+        lendedBook.remove(book);
+    }
+
+    public float getBorrowerRating() {
+        return lenderRating;
+
+    }
+}
