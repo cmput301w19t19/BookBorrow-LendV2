50e7b366856f5497f23cca4c10f5a764be0c5c80
Yuan Tian <yuan@Yuans-MacBook-Pro.local>
2019-02-20 17:16:14 -0700

    add firebase

---
+++ app/src/main/java/com/example/y/bookborrow_lendv2/borrower.java
@@ -0,0 +1,23 @@
+package com.example.y.bookborrow_lendv2;
+
+import java.util.ArrayList;
+
+public class borrower extends user {
+    private float borrowerRating;
+    private ArrayList<book> borrowedBook;
+    private ArrayList<book> requestedBookList;
+
+    borrower(){}
+    public void setBorrowerRating(Float rating){
+        this.borrowerRating = rating;
+    }
+
+
+    public void addBorrowedBook(book book){
+        borrowedBook.add(book);
+    }
+
+    public void addRequestedBook(book book){
+        requestedBookList.add(book);
+    }
+}
---
+++ app/src/main/java/com/example/y/bookborrow_lendv2/lender.java
@@ -0,0 +1,28 @@
+package com.example.y.bookborrow_lendv2;
+
+import java.util.ArrayList;
+
+public class lender extends user {
+    private float lenderRating;
+    private ArrayList<book> lendedBook;
+    private ArrayList<book> requestedBookList;
+
+    lender(){}
+    public void setLenderRating(Float rating){
+        this.lenderRating = rating;
+    }
+
+
+    public void addBorrowedBook(book book){
+        lendedBook.add(book);
+    }
+
+    public void addRequestedBook(book book){
+        requestedBookList.add(book);
+    }
+
+    public void setLenderRating(float lenderRating) {
+        this.lenderRating = lenderRating;
+    }
+}
+
