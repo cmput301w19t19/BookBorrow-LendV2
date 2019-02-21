b6f79ba994078ee2be5396ce93164cf176dcb11f
Yuant9 <yuan@Yuans-MacBook-Pro.local>
2019-02-20 20:42:08 -0700

    update borrower class

--- app/src/test/java/com/example/y/bookborrow_lendv2/borrowerTest.java
+++ app/src/test/java/com/example/y/bookborrow_lendv2/borrowerTest.java
@@ -2,7 +2,10 @@ package com.example.y.bookborrow_lendv2;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
+
 
 public class borrowerTest {
 
@@ -13,10 +16,26 @@ public class borrowerTest {
     @Test
     public void getGet_SetRating(){
         borrower borrower1 = new borrower();
-            borrower1.setBorrowerRating((float)3.3);
-            assertEquals((float)3.3,borrower1.getBorrowerRating(),(float)0.0001);
+        borrower1.setBorrowerRating((float)3.3);
+        assertEquals((float)3.3,borrower1.getBorrowerRating(),(float)0.0001);
 
-        }
+    }
+    @Test   //failed
+    public void deleteBorrowedBook(){
+
+        borrower borrower1 = new borrower();
+        book book1 = new book();
+        ArrayList<book> books = new ArrayList<book>();
+        books.add(book1);
+        borrower1.setBorrowedBook(books);
+        borrower1.deleteBorrowedBook(book1);
+
+
+        assertEquals(0,borrower1.getRequestedBookList().size());
+
+
+    }
+
 
 
 
