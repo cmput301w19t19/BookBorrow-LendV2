84e09e57b3793f251f1a314aec0e345215eac701
Zhouuuw <ninawen2016@gmail.com>
2019-03-08 19:52:59 -0700

    DeleteFromFirebase and new constructor for book; privateBookDetailPage implement

--- app/src/main/java/com/example/y/bookborrow_lendv2/book.java
+++ app/src/main/java/com/example/y/bookborrow_lendv2/book.java
@@ -34,6 +34,10 @@ public class book {
         this.ID = UUID.randomUUID();
     }
 
+    book(String id){
+        this.ID = UUID.fromString(id);
+    };
+
 
     /**
      * This constructor is built for writing unit tests, we will use another constructor which doesn't have parameters
@@ -186,6 +190,10 @@ public class book {
 
     public String getDescriptionBundle(){
         return this.getAuthor()+"\n"+this.getName()+"\n"+this.getISBN();
+    }
+
+    public boolean deleteFromFirebase(){
+        return true;
     }
 }
 
