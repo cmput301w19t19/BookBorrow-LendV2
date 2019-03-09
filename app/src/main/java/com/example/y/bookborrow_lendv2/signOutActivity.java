9655ec49a1b9448dce5bc4b42be48a41a7521f8f
Yuant9 <yt9@ualberta.ca>
2019-03-09 00:30:49 -0700

    reinsert

---
+++ app/src/main/java/com/example/y/bookborrow_lendv2/signOutActivity.java
@@ -0,0 +1,51 @@
+package com.example.y.bookborrow_lendv2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class signOutActivity extends AppCompatActivity {
+
+
+    private Button signOutButton;
+    private FirebaseAuth auth;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_out);
+        signOutButton = (Button) findViewById(R.id.signOutButton);
+
+
+
+
+        Toast.makeText(getApplicationContext(), "currentUser logout userID:"+lender.Instance().getUid(), Toast.LENGTH_SHORT).show();
+
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                signOut();
+                startActivity(new Intent( signOutActivity.this,loginAct.class));
+
+
+            }
+        });
+    }
+
+
+    //sign out method
+    public void signOut() {
+        auth = FirebaseAuth.getInstance();
+
+        auth.signOut();
+    }
+}
