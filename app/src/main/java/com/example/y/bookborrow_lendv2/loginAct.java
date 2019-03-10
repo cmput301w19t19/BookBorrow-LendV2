9e63c23a184809be7302de26d7b2ca243dc6bbb0
Yuant9 <yt9@ualberta.ca>
2019-03-09 23:22:54 -0700

    homepage done

--- app/src/main/java/com/example/y/bookborrow_lendv2/loginAct.java
+++ app/src/main/java/com/example/y/bookborrow_lendv2/loginAct.java
@@ -214,9 +214,9 @@ public class loginAct extends AppCompatActivity {
 
                                     Toast.makeText(getApplicationContext(), "Login Success!", Toast.LENGTH_SHORT).show();
 
-                                    //Intent intent = new Intent(loginAct.this, signOutActivity.class);
-                                    //startActivity(intent);
-                                    //finish();
+                                    Intent intent = new Intent(loginAct.this, home_page.class);
+                                    startActivity(intent);
+                                    finish();
                                 }
                             }
 
