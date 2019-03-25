98de3cfe0f366ab5bdca436d3f16057f9ce135f0
7bw <44212362+Zhouuuw@users.noreply.github.com>
2019-03-25 15:14:09 -0600

    Update RatingAndComment.java

--- app/src/main/java/com/example/y/bookborrow_lendv2/RatingAndComment.java
+++ app/src/main/java/com/example/y/bookborrow_lendv2/RatingAndComment.java
@@ -4,90 +4,60 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 /**
- * The type Rating and comment.
+ *  DO NOT CHANGE ANY INTERFACES FOR THE GETTING AND SETTING
+ *  UNLESS YOU ARE GOING TO COMPLETE THE CHANGES IN OTHER ACTIVITY
+ *
+ *  This class is a container.
+ *  A RatingAndComment can be made to a book, a lender or a borrower:
+ *  A RatingAndComment to a book is for the content:
+ *  Is the book intersting? Is the book attractive?
+ *
+ *  A RatingAndComment to a lender is
+ *  for the copy of the book that the lender rent to others,
+ *  for the communication experience when borroring a lender's book
+ *  and for the lender's attitude toward sharing his or hers book
+ *
+ *  A RatingAndComment to a borrower is for
+ *  the borrower's attitude towards using other's book
+ *  and the communication experience when lending this borrower books
+ *
+ *  ID is the unique ID of any user who tend to make the rate and comment
+ *  A rating is double object BUT THE GETTER FOR RATING RETURNS A STRING FOR SHOING IT IN TEXTVIEW
+ *  A comment is a string with any numbers of words inside
+ *
  */
 public class RatingAndComment {
-    private String borrowerID;
-    private Double borrowerRating;
-    private String comment;
-    //private FirebaseDatabase m = FirebaseDatabase.getInstance();
+    private String ID;
+    private Double rating;
+    private String comment = null;
 
-    /**
-     * Instantiates a new Rating and comment.
-     */
     RatingAndComment(){}
-
-    /*
-    RatingAndComment(String ID){
-        this.setBorrowerID(ID);
+    
+    
+    public void setID(String ID) {
+        this.ID = ID;
     }
 
-    public void setToFirebase(){
-        DatabaseReference r1 = m.getReference("RatingAndComment/"+this.borrowerID);
-        r1.child("borrowerRating").setValue(Double.toString(this.borrowerRating));
-        r1.child("comment").setValue(this.comment);
+    public String getID() {
+        return this.ID;
     }
 
-    public void deleteFromFirebase(){
-        DatabaseReference r = m.getReference("RatingAndComment/").child(this.borrowerID);
-        r.removeValue();
-    }*/
-
-    /**
-     * Sets borrower id.
-     *
-     * @param borrowerID the borrower id
-     */
-    public void setBorrowerID(String borrowerID) {
-        this.borrowerID = borrowerID;
-    }
-
-    /**
-     * Gets borrower id.
-     *
-     * @return the borrower id
-     */
-    public String getBorrowerID() {
-        return borrowerID;
-    }
-
-    /**
-     * Sets borrower rating.
-     *
-     * @param borrowerRating the borrower rating
-     * @return the borrower rating
-     */
-    public boolean setBorrowerRating(Double borrowerRating) {
+    public boolean setRating(Double borrowerRating) {
         if (borrowerRating < 0 || borrowerRating >10){
             return false;
         }
-        this.borrowerRating = borrowerRating;
+        this.rating = borrowerRating;
         return true;
     }
 
-    /**
-     * Gets borrower rating.
-     *
-     * @return the borrower rating
-     */
-    public String getBorrowerRating() {
-        return Double.toString(borrowerRating);
+    public String getRating() {
+        return Double.toString(rating);
     }
 
-    /**
-     * Sets comment.
-     *
-     * @param comment the comment
-     */
     public void setComment(String comment) {
         this.comment = comment;
     }
 
-    /**
-     * Gets comment.
-     *
-     * @return the comment
-     */
     public String getComment() {
         return comment;
     }
